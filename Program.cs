68870ca4b6080d7b6d0f1680af5927a556fcb99e
Hamza DOĞRU <hamzadogru8154@gmail.com>
2021-01-18 22:12:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meyveler = new string[] { };
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+            urun1.StokAdedi = 15;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.StokAdedi = 1;
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            //type-safe--tip güvenli neyle çalışacağımı bilmek isterim 
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.StokAdedi);
+                Console.WriteLine("----------------");
+            }
+            //bu günkü ders
+            Console.WriteLine("--------Metodlar-----------");
+            sepetmaneger sepetmaneger = new sepetmaneger();
+            sepetmaneger.Ekle(urun1);//Metod çağırma
+            sepetmaneger.Ekle(urun2);//Metod çağırma
+
+            sepetmaneger.Ekle2("Armut","Yeşil armut",12);//Metod çağırma
+            sepetmaneger.Ekle2("Elma", "Yeşil elma", 10);
+
+
+        }
+    }
+}
+
+//clean code - Best practise
---
+++ sepetmaneger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class sepetmaneger
+    {
+        public void Ekle(Urun urun)//ilk Urun tipidir. aşağıda ki kodları nerede ne için uygulanacak
+        {
+            Console.WriteLine("Tebrikler,Sepete Eklendi : "+urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string aciklama,double fiyat)
+        {
+            Console.WriteLine("Tebrikler,Sepete Eklendi : " + urunAdi,aciklama,fiyat);
+        }
+    }
+}
